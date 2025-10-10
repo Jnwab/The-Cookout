83877fdcaecad0526aa24f9326b202db03bdb66f
msjulietruong <msjulietruong@gmail.com>
2025-10-10 13:32:58 -0700

    Linked signupScreen with SignupActivity code

--- app/src/main/java/com/cecs491b/thecookout/uiScreens/SignupScreen.kt
+++ app/src/main/java/com/cecs491b/thecookout/uiScreens/SignupScreen.kt
@@ -3,6 +3,7 @@ package com.cecs491b.thecookout.uiScreens
 import androidx.compose.runtime.Composable
 
 @Composable
-fun SignupScreen(){
+fun SignupScreen(    onSignupClick: (email: String, password: String, displayName: String) -> Unit = {_, _, _ -> },
+                     onBackToLoginClick: () -> Unit = {}){
     // will add stuff
 }
