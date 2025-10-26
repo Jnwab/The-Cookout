528b444a847562b31ef25b28b2ebd90fb9f6b73a
Jnwab <85961355+Jnwab@users.noreply.github.com>
2025-10-26 00:31:33 -0700

    Modernized Forgot Password Page
    Simply updated the UI aspects of both SignUp And ForgotPassword in preparations of Homepage implementation

--- app/src/main/java/com/cecs491b/thecookout/uiScreens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/cecs491b/thecookout/uiScreens/ForgotPasswordScreen.kt
@@ -1,6 +1,10 @@
 package com.cecs491b.thecookout.uiScreens
 
+import android.graphics.Paint
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -8,8 +12,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,44 +33,68 @@ fun ForgotPasswordScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Forgot Password") }
+                title = { Text("Forgot Password") },
+            )
+            HorizontalDivider(
+                modifier = Modifier,
+                thickness = 4.dp,
+                color = Color.Black
             )
         }
     ) { inner ->
+
         Column(
-            modifier = Modifier
+            verticalArrangement = Arrangement.Center ,modifier = Modifier
+                .fillMaxSize()
                 .padding(inner)
-                .padding(24.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .verticalScroll(rememberScrollState(),)
         ) {
-            Text(
-                "Enter the email you used to sign up. We’ll send a password reset link.",
-                style = MaterialTheme.typography.bodyMedium
-            )
-
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Done
-                ),
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Button(
-                onClick = { onSendReset(email) },
-                modifier = Modifier.fillMaxWidth()
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                shape = RoundedCornerShape(20.dp),
+                elevation = CardDefaults.cardElevation(16.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
             ) {
-                Text("Send reset email")
-            }
 
-            TextButton(onClick = onClose) {
-                Text("Back to Login")
+                Column(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        "Enter the email you used to sign up. We’ll send a password reset link.",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Email,
+                            imeAction = ImeAction.Done
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Button(
+                        onClick = { onSendReset(email) },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Send reset email")
+                    }
+
+                    TextButton(onClick = onClose) {
+                        Text("Back to Login")
+                    }
+                }
             }
         }
     }
