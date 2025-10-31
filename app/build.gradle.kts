5bdc80d9aea8417e74e4fe18e9ccc2f5bd37a123
Jnwab <85961355+Jnwab@users.noreply.github.com>
2025-10-31 12:11:18 -0700

    Merged Account and UI
    Merged Ui and Account

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
+    implementation("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     //implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
