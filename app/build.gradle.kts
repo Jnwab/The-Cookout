fe01187a50b9a5d2094ff1733eb5fe33b6a25d35
Arnav Mishra <arnav9@outlook.com>
2025-11-11 18:48:54 -0800

    updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.foundation)
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material:material-icons-core:1.5.4")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     //implementation(libs.firebase.auth.ktx)
